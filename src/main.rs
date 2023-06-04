bcc94f8ad42923ac72882bfd61ea94ba320e5e02
Hyeon Kim <simnalamburt@gmail.com>
2023-06-04 15:27:33 +0900

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,81 @@
-use std::io::{stdin, stdout, BufRead, Write};
-use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::io::{stdin, stdout, BufRead, Result, Write};
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
+    program(stdin().lock(), stdout().lock())
+}
+
+fn program<R: BufRead, W: Write>(input: R, mut output: W) -> std::io::Result<()> {
     let mut set = HashMap::new();
 
-    // for each lines of stdin
-    let mut stdout = stdout().lock();
-    for line in stdin().lock().lines() {
+    for line in input.lines() {
         let line = line?;
 
         match set.entry(line) {
             Entry::Occupied(_) => continue,
             Entry::Vacant(e) => {
-                stdout.write_all(e.key().as_bytes())?;
-                stdout.write_all(b"\n")?;
+                output.write_all(e.key().as_bytes())?;
+                output.write_all(b"\n")?;
                 e.insert(());
             }
         }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::program;
+
+    fn test(input: &[u8], expected: &[u8]) {
+        let mut actual = Vec::new();
+        program(&input[..], &mut actual).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_abc() {
+        test(
+            b"\
+aaa
+aaa
+aaa
+bbb
+bbb
+bbb
+ccc
+ccc
+ccc
+",
+            b"\
+aaa
+bbb
+ccc
+",
+        )
+    }
+
+    #[test]
+    fn test_add() {
+        test(
+            b"\
+a
+b
+c
+a
+b
+c
+a
+b
+c
+",
+            b"\
+a
+b
+c
+",
+        )
+    }
 }
